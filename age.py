0827e7ad9512d0df1f9e1900e4b05f9160cde8c8
sharmilapalanivel <42063250+sharmilapalanivel@users.noreply.github.com>
2019-09-23 15:19:59 +0530

    Update age.py

--- age.py
+++ age.py
@@ -6,62 +6,9 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
-image=cv2.imread("./train_5.png")
+image=cv2.imread("./training_img/staff.jpg")
 
-agender = PyAgender()
-faces = agender.detect_genders_ages(image)
-
-ages = round(faces[0]["age"])
-gend=(faces[0]["gender"])
-
-detector = FER()
-res = detector.detect_emotions(image)
-
-angry = res[0]["emotions"]["angry"]
-disgust = res[0]["emotions"]["disgust"]
-fear = res[0]["emotions"]["fear"]
-happy = res[0]["emotions"]["happy"]
-sad = res[0]["emotions"]["sad"]
-surprise = res[0]["emotions"]["surprise"]
-neutral = res[0]["emotions"]["neutral"]
-
-print("************************")
-print("Age : ",round(faces[0]["age"]))
-
-if gend > 0.5:
-	print("Gender : female")
-	gender="Female"
-
-else:
-	print("Gender : male")
-	gender="Male"
-
-if angry > 0.5:
-	print("Emotion : Angry")
-	emo="Angry"
-elif disgust > 0.5:
-	print("Emotion : Disgust")
-	emo="Disgust"
-elif fear > 0.5:
-	print("Emotion : Fear")
-	emo="Fear"
-elif happy > 0.5:
-	print("Emotion : Happy")
-	emo="Happy"
-elif sad > 0.5:
-	print("Emotion : Sad")
-	emo="Sad"
-elif surprise > 0.5:
-	print("Emotion : Surprise")
-	emo="Surprise"
-else:
-	print("Emotion : Neutral")
-	emo="Neutral"
-print("************************")
-#cv2.imwrite("sampleimage.jpg",image)
-
-
-cascPath = "haarcascade_frontalface_default.xml"
+cascPath = 'haarcascade_frontalface_default.xml'
 	
 	# Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
@@ -78,6 +25,72 @@ faces = faceCascade.detectMultiScale(
 	minSize=(30, 30),
 	#flags = cv2.cv.CV_HAAR_SCALE_IMAGE
 )
+count=0
+for (x, y, w, h) in faces: 
+    #cv2.rectangle(image, (x, y), (x+w, y+h), (0, 256, 0), 2)
+    count=count+1
+print(count)
+print("*****")
+for i in range(count):
+    agender = PyAgender()
+    face = agender.detect_genders_ages(image)
+    detector = FER()
+    res = detector.detect_emotions(image)
+#print(face)
+ages=[]
+gend=[]
+genderr=[]
+disgust=[]
+angry=[]
+fear=[]
+sad=[]
+surprise=[]
+neutral=[]
+happy=[]
+emo=[]
+for j in range(count):
+    ages.append(round(face[j]["age"]))
+    gend.append(face[j]["gender"])
+    print(ages[j])
+    if gend[j] > 0.5:
+	    print("Gender : female")
+	    genderr.append("Female")
+
+    else:
+	    print("Gender : male")
+	    genderr.append("Male")
+    angry.append(res[j]["emotions"]["angry"])
+    disgust.append(res[j]["emotions"]["disgust"])
+    fear.append(res[j]["emotions"]["fear"])
+    happy.append(res[j]["emotions"]["happy"])
+    sad.append(res[j]["emotions"]["sad"])
+    surprise.append(res[j]["emotions"]["surprise"])
+    neutral.append(res[j]["emotions"]["neutral"])
+    if angry[j] > 0.5:
+	    print("Emotion : Angry")
+	    emo.append("Angry")
+    elif disgust[j] > 0.5:
+	    print("Emotion : Disgust")
+	    emo.append("Disgust")
+    elif fear[j] > 0.5:
+	    print("Emotion : Fear")
+	    emo.append("Fear")
+    elif happy[j] > 0.5:
+	    print("Emotion : Happy")
+	    emo.append("Happy")
+    elif sad[j] > 0.5:
+	    print("Emotion : Sad")
+	    emo.append("Sad")
+    elif surprise[j] > 0.5:
+	    print("Emotion : Surprise")
+	    emo.append("Surprise")
+    elif neutral[j]>0.5:
+	    print("Emotion : Neutral")
+	    emo.append("Neutral")
+    else:
+        print("no")
+#print(emo)
+
 font = cv2.FONT_HERSHEY_SIMPLEX 
   
 # org 
@@ -90,17 +103,18 @@ fontScale = 1
 color = (0, 0, 255) 
   
 # Line thickness of 2 px 
-thickness = 4
-
-
-text="{}, {}, {}".format(ages,gender,emo)
-	
+thickness = 2
+text=[]
+for i in range(count):
+    text.append("{},{},{}".format(ages[i],genderr[i],emo[i]))
+#print(text)	
 	# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
+
+for (x, y, w, h),j in zip(faces,range(count)): 
 	cv2.rectangle(image, (x, y), (x+w, y+h), (0, 256, 0), 2)
-	cv2.putText(image,text ,(x, y), font, fontScale,color, thickness, cv2.LINE_AA, False)
+	cv2.putText(image,text[j] ,(x, y), font, fontScale,color, thickness, cv2.LINE_AA, False)
 # display the output image
 cv2.namedWindow("face detection",cv2.WINDOW_NORMAL)
-imS = cv2.resize(image,(2000,2000),thickness)
+imS = cv2.resize(image,(2000,2000))
 cv2.imshow("face detection",imS)
 cv2.waitKey(0)
